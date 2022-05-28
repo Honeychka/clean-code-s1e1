5c45196f1ad553be815a27977143ca4069ae995c
Hanna Khanko <ahanko802@gmail.com>
2022-05-28 13:01:34 +0300

    refactor: adjust formulaic naming style for selectors

--- app.js
+++ app.js
@@ -41,9 +41,9 @@ var createNewTaskElement=function(taskString){
     editInput.className="task";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="task-behaviour";
 
-    deleteButton.className="delete";
+    deleteButton.className="task-remover";
     deleteButtonImg.src='./remove.svg';
     deleteButton.appendChild(deleteButtonImg);
 
@@ -84,7 +84,7 @@ var editTask=function(){
 
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
-    var editBtn=listItem.querySelector(".edit");
+    var editBtn=listItem.querySelector(".task-behaviour");
     var containsClass=listItem.classList.contains("edit-tasks");
     //If class of the parent is .editmode
     if(containsClass){
@@ -156,8 +156,8 @@ var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
     console.log("bind list item events");
 //select ListItems children
     var checkBox=taskListItem.querySelector("input[type=checkbox]");
-    var editButton=taskListItem.querySelector("button.edit");
-    var deleteButton=taskListItem.querySelector("button.delete");
+    var editButton=taskListItem.querySelector("button.task-behaviour");
+    var deleteButton=taskListItem.querySelector("button.task-remover");
 
 
     //Bind editTask to edit button.
